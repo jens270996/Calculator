fa25e904067b33c61161a9320dca5d3b28a95044
Jens Kristensen <jens-nk@hotmail.com>
2020-09-10 13:31:20 +0200

    rename

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -5,24 +5,73 @@ namespace Calculatorspace
  
         public class Calculator
         {
-            public Calculator() { }
+            public Calculator()
+            {
+                Accumulator = Double.NaN;
+            }
 
             public double Add(double a, double b)
             {
-                return a + b;
+                return Accumulator=a + b;
+            }
+
+            public double Add(double a)
+            {
+                return Accumulator += a;
             }
 
             public double Subtract(double a, double b)
             {
-                return a - b;
+                return Accumulator=a - b;
+            }
+
+            public double Subtract(double a)
+            {
+                return Accumulator -= a;
             }
             public double Multiply(double a, double b)
             {
-                return a * b;
+                return Accumulator=a * b;
+            }
+
+            public double Multiply(double a)
+            {
+                return Accumulator*=a;
             }
             public double Power(double x, double exp)
             {
-                return Math.Pow(x, exp);
+                return Accumulator=Math.Pow(x, exp);
+            }
+
+            double Power(double exp)
+            {
+                return Power(Accumulator, exp);
+            }
+
+            public double Divide(double dividend, double divisor)
+            {
+                if (divisor == 0)
+                {
+                    Accumulator=Double.NaN;
+                    throw new DivideByZeroException();
+                }
+                
+                else
+                {
+                    return Accumulator=dividend / divisor;
+                }
+            }
+
+            public double Divide(double divisor)
+            {
+                return Divide(Accumulator, divisor);
+
+            }
+            public double Accumulator { get; private set; }
+
+            public void Clear()
+            {
+                Accumulator = 0;
             }
         }
 
